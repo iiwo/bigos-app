d3ca65dc6d9b06652b5911aa608b88bc59a2e5a8
kremenso <kremenso@o2.pl>
2013-01-24 21:59:43 +0100

    comment setting

--- app/models/bigos_app/page_element.rb
+++ app/models/bigos_app/page_element.rb
@@ -2,7 +2,7 @@ module BigosApp
 
   class PageElement < ActiveRecord::Base
 
-    include RailsSettings::Extend
+    #include RailsSettings::Extend
 
     attr_accessible :is_admin_visible, :name, :type, :page_id, :position,
                     :text_block_attributes, :gallery_attributes, :product_category_attributes
