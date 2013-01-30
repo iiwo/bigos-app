a5c8d833d9de2df39b4a0d5f83ae038b03fc100e
kremenso <kremenso@o2.pl>
2013-01-30 20:50:20 +0100

    admin

--- app/assets/javascripts/bigos_app/admin/pages.js
+++ app/assets/javascripts/bigos_app/admin/pages.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    $(this).closest("fieldset").remove()
     $("a.remove_exist").on("click", function(){
         console.log("remove_exist click")
       $(this).closest("fieldset").remove()
