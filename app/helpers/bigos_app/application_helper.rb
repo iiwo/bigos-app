5123298e55b8da73ca145b42fdd743066107a686
kremenso <kremenso@o2.pl>
2013-02-04 15:38:35 +0100

    justifycenter

--- app/helpers/bigos_app/application_helper.rb
+++ app/helpers/bigos_app/application_helper.rb
@@ -1,12 +1,5 @@
 module BigosApp
   module ApplicationHelper
 
-    def get_gallery_images_json gallery
-      images_tab = Array.new
-      gallery.gallery_images.each do |bi|
-        images_tab.push({'image' => bi.image.url(:medium), 'title' => "", 'thumb' => bi.image.url(:thumb)})
-      end
-      images_tab.to_json
-    end
   end
 end
